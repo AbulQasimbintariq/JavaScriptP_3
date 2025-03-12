5da5ae39df6fb42adc7e7a1ddfe4236225812d9b
AbulQasimbintariq <www.qasimbhalli6547@gmail.com>
2025-03-13 00:09:27 +0500

    add file

---
+++ app.js
@@ -0,0 +1,39 @@
+let Operators_ = document.querySelectorAll(".Operators")
+let val1=document.querySelector("#val_1")
+let val2=document.querySelector("#val_2")
+let Ans_=document.querySelector("#Ans")
+
+// console.log(val1.value)
+
+// let Ans=0
+
+// function calc(Operators_){
+//  Operators_.forEach(()=>{
+//         if(Operators_[1]){
+//             // console.log(Operators_.index)
+//            Ans=val1.value+val2.value 
+//            console.log(Ans)    
+        // } else if(Operator==Operators_[2]){
+        //     Ans=val1.value-val2.value 
+        //     console.log(Ans)
+        // } else if(Operator==Operators_[3]){
+        //     Ans=val1.value*val2.value 
+        //     console.log(Ans) 
+        // } else if(Operator==Operators_[4]){
+        //     Ans=val1.value/val2.value 
+        //     console.log(Ans)
+        // } else if(Operator==Operators_[5]){
+        //     Ans=val1.value%val2.value 
+        //     console.log(Ans)
+        // } else if(Operator==Operators_[6]){
+        //     Ans=val1.value**val2.value 
+        //     console.log(Ans)
+        // } else(Operator==Operators_[7])
+        // {
+        //     Ans=val1.value/val2*100
+        //     console.log(Ans)
+        // }                  
+//     }
+// })
+// }
+
